cb791bc05e00c6f13b2acad8aaa44f4262d87d78
Ndav07 <nikollasdavidor@hotmail.com>
2022-11-23 11:04:39 -0300

    feat: ensuri integity in transfer

--- src/bank-accounts/bank-accounts.service.ts
+++ src/bank-accounts/bank-accounts.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 
-import { Repository } from 'typeorm'
-import { InjectRepository } from '@nestjs/typeorm'
+import { DataSource, Repository } from 'typeorm'
+import { getDataSourceToken, InjectRepository } from '@nestjs/typeorm'
 
 import { BankAccount } from './entities/bank-account.entity'
 import { CreateBankAccountDto } from './dto/create-bank-account.dto'
@@ -10,7 +10,9 @@ import { CreateBankAccountDto } from './dto/create-bank-account.dto'
 export class BankAccountsService {
   constructor(
     @InjectRepository(BankAccount)
-    private readonly repository: Repository<BankAccount>
+    private readonly repository: Repository<BankAccount>,
+    @Inject(getDataSourceToken())
+    private readonly dataSource: DataSource
   ) {}
 
   async create(createBankAccountDto: CreateBankAccountDto) {
@@ -31,15 +33,23 @@ export class BankAccountsService {
   }
 
   async transfer(from: string, to: string, amount: number) {
-    const fromAccount = await this.repository.findOneBy({
-      account_number: from,
-    })
-    const toAccount = await this.repository.findOneBy({ account_number: to })
+    const queryRunner = this.dataSource.createQueryRunner()
+    try {
+      await queryRunner.startTransaction()
+      const fromAccount = await this.repository.findOneBy({
+        account_number: from,
+      })
+      const toAccount = await this.repository.findOneBy({ account_number: to })
 
-    fromAccount.balance -= amount
-    toAccount.balance += amount
+      fromAccount.balance -= amount
+      toAccount.balance += amount
 
-    this.repository.save(fromAccount)
-    this.repository.save(toAccount)
+      this.repository.save(fromAccount)
+      this.repository.save(toAccount)
+      await queryRunner.commitTransaction()
+    } catch (err) {
+      await queryRunner.rollbackTransaction()
+      throw err
+    }
   }
 }
